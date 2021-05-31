01a4eff45055ddd50ca8a51bc3889f41a7e34aa4
sirib815 <47656265+sirib815@users.noreply.github.com>
2021-05-31 18:07:48 +0530

    Junit Activities

---
+++ junit/BankAccount.java
@@ -0,0 +1,27 @@
+
+public class BankAccount {
+	
+	    private Integer balance;
+	    
+	    // Create a constructor
+	   // public BankAccount(Integer initialBalance) {
+	       // balance = initialBalance;
+	    //}
+
+		public BankAccount(Integer initialBalance) {
+			balance = initialBalance;
+			// TODO Auto-generated constructor stub
+		}
+
+		// Add method to calculate
+	    // balance amount after withdrawal
+	    public Integer withdraw(Integer amount) {
+	        if (balance < amount) {
+	            throw new NotEnoughFundsException(amount, balance);
+	        }
+	        balance -= amount;
+	        return balance;
+	    }
+	}
+
+
